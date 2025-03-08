01df84760856293ca2bdb6032cf315dbb5cff98c
unknown <muhayminul96@gmail.com>
2025-03-09 00:26:08 +0600

    update roman to int

--- 13 roman to integer.py
+++ 13 roman to integer.py
@@ -1,30 +1,24 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_to_int = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
+        roman_values = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
         }
-        strl = list(s)
-        maxi = 0
-        maxi_R = ''
-        for index,value in enumerate(strl):
-            if roman_to_int[value] >= maxi:
-                maxi = roman_to_int[value]
-                maxi_R = value
 
-        start = strl.index(maxi_R)
-        sum = 0
-        for i in strl[start:]:
-            sum = sum + roman_to_int[i]
-        for i in strl[:start]:
-                sum = sum - roman_to_int[i]
+        total = 0
+        prev_value = 0
 
-        print(sum)
+        for char in reversed(s):  # Process from right to left
+            value = roman_values[char]
+
+            if value < prev_value:
+                total -= value  # Subtraction case
+            else:
+                total += value  # Addition case
+
+            prev_value = value
+
+        return total
 
 
 
---
+++ 3471. Find the Largest Almost Missing Integer.py
@@ -0,0 +1,4 @@
+class Solution:
+    def largestInteger(self, nums: List[int], k: int) -> int:
+        max_num = max(nums)
+        min_num = min(nums)
